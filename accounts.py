3f921c7472c23d0b7e6e638eaca31342544af13c
Muazan <108673162+muazan1@users.noreply.github.com>
2025-01-04 22:05:13 +0500

    initail commit

---
+++ accounts.py
@@ -0,0 +1,1 @@
+[{'name': 'Muazan', 'cnic': '123456789', 'pin': '3333', 'balance': 0, 'transactions': []}, {'name': 'Noman', 'cnic': '321321', 'pin': '3333', 'balance': 0, 'transactions': []}, {'name': 'Muazan', 'cnic': '123456', 'pin': '3363', 'balance': 0, 'transactions': []}]
